183e11499cea699ce2ee88ecd897aee50868aa8a
aulia <nurulmaulidiyah784@gmail.com>
2025-09-22 12:13:09 +0700

    Menambahkan file java pertama

--- JurnalNurul2.java
+++ JurnalNurul2.java
@@ -1,5 +1,5 @@
 public class JurnalNurul2 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main (String[] args) {
+        System.out.println ("Hello, World!");
     }
 }
